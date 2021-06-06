284af2bf773fa9637c1dd879b5ba3585be173e5f
Tomohiro Nishimura <tomohiro68@gmail.com>
2021-06-06 09:41:25 +0900

    initial commit

---
+++ lib/method_annotation.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "method_annotation/version"
+
+module MethodAnnotation
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/method_annotation/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MethodAnnotation
+  VERSION = "0.1.0"
+end
